eb62779f53278d472a6c46b0b750134803ce998c
Daniela Sánchez Nizza <83455471+Dani1i@users.noreply.github.com>
2023-10-19 15:19:24 -0300

    Update recipes.jsx

--- src/pages/recipes.jsx
+++ src/pages/recipes.jsx
@@ -8,6 +8,8 @@ import LogoUSM from '../assets/usm.png'
 
 export const RecipesPage = () => {
   const url = "https://i.blogs.es/927bbe/salsa-pasta/840_560.jpg"
+  const url2 = "https://tofuu.getjusto.com/orioneat-local/resized2/zRZZx7uobtDFtm69o-1200-1200.webp"
+  const url3="https://cdn0.recetasgratis.net/es/posts/6/2/7/avena_con_leche_73726_600_square.jpg"
   return (
     <Container>
       <NavBar />
@@ -19,6 +21,23 @@ export const RecipesPage = () => {
           <a href="/receta" class="btn btn-info" role="button" style={{textDecoration: 'none'}}>FIDEOS CON SALSA BOLOÑESA</a>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'row', backgroundColor: '#E3FFC8', padding: '25px', borderRadius: '15px', alignItems: 'flex-start'}}>
+        <img src={url2} style={{width: '150px', height: '150px', marginRight: '25px', borderRadius: '15px'}}/>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <a href="/receta" class="btn btn-info" role="button" style={{textDecoration: 'none'}}>POLLO CON PAPAS</a>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        
+        
+      </div>
+      <div style={{display: 'flex', flexDirection: 'row', backgroundColor: '#E3FFC8', padding: '25px', borderRadius: '15px', alignItems: 'flex-start'}}>
+        <img src={url3} style={{width: '150px', height: '150px', marginRight: '25px', borderRadius: '15px'}}/>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <a href="/receta" class="btn btn-info" role="button" style={{textDecoration: 'none'}}>AVENA</a>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        
         
       </div>
       
